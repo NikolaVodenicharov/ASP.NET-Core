d3871c254000d2390c9d77e6a0300af867054d02
nikola <nikolavodeni4arov@gmailcom>
2018-09-15 10:24:57 +0300

    Add Users

--- CarDealer/CarDealer.Web/Startup.cs
+++ CarDealer/CarDealer.Web/Startup.cs
@@ -28,7 +28,13 @@ namespace CarDealer.Web
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services
-                .AddIdentity<User, IdentityRole>()
+                .AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
                 .AddEntityFrameworkStores<CarDealerDbContext>()
                 .AddDefaultTokenProviders();
 
